b238906fefcb09a606f41f2128f8195b450af06d
Alexander Shepetko <a@shepetko.com>
2017-01-05 21:33:39 +0200

    Release 0.2.3

--- README.md
+++ README.md
@@ -2,6 +2,11 @@
 
 ## Changelog
 
+### 0.2.3 (2017-01-05)
+- Translations fixed.
+- plugin.json updated
+
+
 ### 0.2.2 (2016-12-19)
 plugin.json updated.
 
--- __init__.py
+++ __init__.py
@@ -1,36 +1,33 @@
 """PytSite Twitter Plugin.
 """
+from pytsite import assetman as _assetman, lang as _lang, permissions as _permissions, settings as _settings, \
+    events as _events
+from . import _settings_form, _content_export, _eh
+
 __author__ = 'Alexander Shepetko'
 __email__ = 'a@shepetko.com'
 __license__ = 'MIT'
 
+# Resources
+_lang.register_package(__name__, alias='twitter')
+_assetman.register_package(__name__, alias='twitter')
 
-def _init():
-    from pytsite import assetman, lang, permissions, settings, events
-    from . import _settings_form, _content_export, _eh
+# Lang globals
+_lang.register_global('twitter_admin_settings_url', lambda language, args: _settings.form_url('twitter'))
 
-    # Resources
-    lang.register_package(__name__, alias='twitter')
-    assetman.register_package(__name__, alias='twitter')
+# Permissions
+_permissions.define_permission('twitter.settings.manage', 'twitter@manage_twitter_settings', 'app')
 
-    # Lang globals
-    lang.register_global('twitter_admin_settings_url', lambda: settings.form_url('twitter'))
+# Settings
+_settings.define('twitter', _settings_form.Form, 'twitter@twitter', 'fa fa-twitter', 'twitter.settings.manage')
 
-    # Permissions
-    permissions.define_permission('twitter.settings.manage', 'twitter@manage_twitter_settings', 'app')
+# Content export driver
+try:
+    from plugins import content_export
 
-    # Settings
-    settings.define('twitter', _settings_form.Form, 'twitter@twitter', 'fa fa-twitter', 'twitter.settings.manage')
+    content_export.register_driver(_content_export.Driver())
+except ImportError as e:
+    raise RuntimeError("Required plugin is not found: {}".format(e))
 
-    # Content export driver
-    try:
-        from plugins import content_export
-        content_export.register_driver(_content_export.Driver())
-    except ImportError as e:
-        raise RuntimeError("Required plugin is not found: {}".format(e))
-
-    # Event handlers
-    events.listen('pytsite.router.dispatch', _eh.router_dispatch)
-
-
-_init()
+# Event handlers
+_events.listen('pytsite.router.dispatch', _eh.router_dispatch)
